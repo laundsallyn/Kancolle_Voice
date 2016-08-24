fc0e70f7bc8dd3018814366a760bd9e84d7c5381
Yipu Wang <yipu.wang@utexas.edu>
2016-08-24 14:40:03 -0500

    notification action still not working

--- app/src/main/java/yipuwang/kancolle/MyNotification.java
+++ app/src/main/java/yipuwang/kancolle/MyNotification.java
@@ -6,7 +6,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-import android.widget.RemoteViews;
 
 /**
  * Created by Yipu Wang on 8/23/2016.
@@ -14,20 +13,20 @@ import android.widget.RemoteViews;
 public class MyNotification {
     public MyNotification (Context con, Character name){
         NotificationCompat.Builder bud = new NotificationCompat.Builder(con);
-        bud.setSmallIcon(R.mipmap.haruna_icon);
+        bud.setSmallIcon(R.drawable.noticon_xhdpi);
         bud.setContentTitle("Mobile Secretary Ship");
         bud.setContentText("Current secretary ship: "+ name.name());
-        Intent resultIntent = new Intent(con, MainActivity.class);
-        resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        // Because clicking the notification opens a new ("special") activity, there's
-        // no need to create an artificial back stack.
-        PendingIntent resultPendingIntent =
-                PendingIntent.getActivity(
-                        con,
-                        0,
-                        resultIntent,
-                        PendingIntent.FLAG_NO_CREATE
-                );
+//        Intent resultIntent = new Intent(con, MainActivity.class);
+//        resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+//        // Because clicking the notification opens a new ("special") activity, there's
+//        // no need to create an artificial back stack.
+//        PendingIntent resultPendingIntent =
+//                PendingIntent.getActivity(
+//                        con,
+//                        0,
+//                        resultIntent,
+//                        PendingIntent.FLAG_NO_CREATE
+//                );
 //        bud.setContentIntent(resultPendingIntent);
         Notification noti = bud.build();
         NotificationManager man = (NotificationManager) con.getSystemService(Context.NOTIFICATION_SERVICE);
